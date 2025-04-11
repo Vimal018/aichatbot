0ea04a7ac291999c3204c85bc4984df80c8190c6
Vimal <vimal2409it@gmail.com>
2025-04-11 14:02:45 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -96,7 +96,6 @@
 
 import { Box, useMediaQuery, useTheme } from "@mui/material";
 //import React from "react";
-import TypingAnim from "../components/typer/TypingAnim";
 import Footer from "../components/footer/Footer";
 
 const Home = () => {
